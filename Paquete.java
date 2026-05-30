d054eefeaee287d2f83e9af4c10508a89632b586
Katu Crei <katucrei.git@gmail.com>
2026-05-30 00:26:10 -0300

    clase paquete

---
+++ Paquete.java
@@ -0,0 +1,31 @@
+public class Paquete{
+    private int id;
+    private String codigoId;
+    private double peso;
+    private boolean tieneAlimentos;
+    private int valorUrgencia;
+
+    public Paquete(int id, String codigoId, double peso, boolean tieneAlimentos, int valorUrgencia){
+        this.id= id;
+        this.codigoId= codigoId;
+        this.peso= peso;
+        this.tieneAlimentos= tieneAlimentos;
+        this.valorUrgencia= valorUrgencia;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+    public String getCodigoId(){
+        return this.codigoId;
+    }
+    public double getPeso(){
+        return this.peso;
+    }
+    public boolean getTieneAlimentos(){
+        return this.tieneAimentos;
+    }
+    public int valorUrgencia(){
+        retun this.valorUrgencia;
+    }
+}
